ae64610d4a7290b00a107cd601f6e3652c487633
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-08-18 09:18:14 +0300

    Create giphy.js

---
+++ Cmds/giphy.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const { colors } = require("../utils.json");
+const request = require("node-superfetch");
+module.exports.run = async (bot, msg, args) => {
+    try {
+        let query = args.join(" ");
+        const { body } = await request
+				.get('http://api.giphy.com/v1/gifs/search')
+				.query({
+					q: query,
+					api_key: process.env.API_GIPHY_KEY,
+					rating: msg.channel.nsfw ? 'r' : 'pg'
+                });
+                if (!body.data.length) return msg.channel.sendEmbed(new Discord.RichEmbed().setDescription("Ничего не найдено."));
+                var embed = new Discord.RichEmbed()
+                .setImage(body.data[Math.floor(Math.random() * body.data.length)].images.original.url)
+                .setColor("RANDOM")
+			msg.channel.send(embed)
+    } catch (error) {
+        msg.channel.sendEmbed(new Discord.RichEmbed().setTitle("Произошла ошибка").setDescription(error).setColor(colors.err))
+    }
+}
+
+module.exports.help = {
+    name: "gif"
+}
