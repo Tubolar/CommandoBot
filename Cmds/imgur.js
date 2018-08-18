306d2dba4435a76ca69e9ffc735424ad8772a520
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-08-18 09:23:07 +0300

    Create imgur.js

---
+++ Cmds/imgur.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const { colors } = require("../utils.json");
+const request = require("node-superfetch");
+module.exports.run = async (bot, msg, args) => {
+    try {
+        const query = args.join(" ");
+        const { body } = await request
+				.get('https://api.imgur.com/3/gallery/search')
+				.query({ q: query })
+				.set({ Authorization: `Client-ID ${process.env.API_IMGUR_KEY}` });
+			var images = body.data.filter(image => image.images && (msg.channel.nsfw ? true : !image.nsfw));
+            if (!images.length) return msg.channel.sendEmbed(new Discord.RichEmbed().setDescription("Ничего не найдено"));
+            var attachment =`${images[Math.floor(Math.random() * images.length)].images[0].link}`
+            msg.channel.send({file: attachment})
+    } catch (error) {
+        msg.channel.sendEmbed(new Discord.RichEmbed().setTitle("Произошла ошибка").setDescription(error).setColor(colors.err))
+    }
+    
+}
+
+module.exports.help = {
+    name: "imgur"
+}
