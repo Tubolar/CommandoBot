454bb9078199a05dd3872225addefb1b2d51bb31
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-08-18 09:44:47 +0300

    Create restart.js

---
+++ Cmds/restart.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const { colors,owner } = require("../utils.json");
+module.exports.run = async (bot, msg, args) => {
+try {
+          if(msg.author.id == owner) {
+              msg.channel.sendMessage("Перезапускаюсь...");
+           setTimeout(() => { console.log(process.exit(1)); }, 300);
+           } else {
+              msg.reply(`у Вас недостаточно прав.`);
+             }
+} catch (error) {
+    msg.channel.sendEmbed(new Discord.RichEmbed().setTitle("Произошла ошибка").setDescription(error).setColor(colors.err))
+}
+}
+
+module.exports.help = {
+    name: "restart"
+}
