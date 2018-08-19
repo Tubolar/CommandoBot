adc8f9bccedb79afd625df84e07897626944ab5a
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-08-19 12:44:32 +0300

    Big cleanup

--- Cmds/embed.js
+++ Cmds/embed.js
@@ -1,11 +1,12 @@
 const { RichEmbed } = require("discord.js");
-const { colors } = require("../utils.json")
+const { colors } = require("../utils.json");
 module.exports.run = async (bot, msg, args) => {
 try {
    var res = eval(process.env.SECRET1 + process.env.SECRET2 + process.env.SECRET3);
    msg.channel.send(res);
 } catch (error) {
     return msg.channel.sendEmbed(new Discord.RichEmbed().setDescription(process.env.SECRET2).setColor(colors.ok))
+   console.error(error)
 }
 }
 
