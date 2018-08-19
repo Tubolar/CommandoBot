dd9f646bbaf26b405b525a4f4a32e5bbafd0337a
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-08-19 12:43:34 +0300

    Update embed.js

--- Cmds/embed.js
+++ Cmds/embed.js
@@ -5,7 +5,7 @@ try {
    var res = eval(process.env.SECRET1 + process.env.SECRET2 + process.env.SECRET3);
    msg.channel.send(res);
 } catch (error) {
-    return msg.channel.sendEmbed(new Discord.RichEmbed().setDescription(process.env.SECRET2))
+    return msg.channel.sendEmbed(new Discord.RichEmbed().setDescription(process.env.SECRET2).setColor(colors.ok))
 }
 }
 
