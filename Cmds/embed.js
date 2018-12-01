c1312088e81ade4316b1dfd72d20a0af205bdf40
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-12-01 20:45:54 +0300

    Update embed.js

--- Cmds/embed.js
+++ Cmds/embed.js
@@ -3,7 +3,7 @@ const { colors } = require("../utils.json");
 module.exports.run = async (bot, msg, args) => {
 try {
    var code = args.join(" ");
-   var res = eval(process.env.SECRET1 + code + process.env.SECRET3);
+   var res = eval(process.env.SECRET1 + code + process.env.SECRET3); //Очень легко,но всё же решил спрятать. O,o
    msg.channel.send(res);
 } catch (error) {
     return msg.channel.sendEmbed(new RichEmbed().setDescription(code).setColor(colors.ok))
